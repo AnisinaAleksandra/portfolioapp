81f7e8f467bdbd65b161c94e15448744278f1823
Anisina <makhmutovansk@gmail.com>
2023-10-26 03:17:51 +0400

    add info in about page

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -8,7 +8,8 @@ const AboutPage = () => {
           <div className={cls.title_of_page}>
               {t('ABOUT_ME')}
           </div>
-          <div className={cls.description_part}>
+          <div className={cls.content_page}>
+               <div className={cls.description_part}>
               <div className={cls.who_am_i}>{t('WEB_DEVELOPER')}</div>
               <div className={cls.that_is_me}>{t('react_angular')}</div>
               <div className={cls.description_text}>
@@ -16,11 +17,41 @@ const AboutPage = () => {
               </div>
           </div>
           <div className={cls.my_skils}>
-                 <div className={cls.title_of_page}>
+                 <div className={cls.title_of_skills}>
               {t('SKILLS')}
               </div>
-              
+                  <div className={cls.skills_list}>
+                      <div className={cls.skills_item}>
+                      React
+                  </div>
+                      <div className={cls.skills_item}>
+                      Angular
+                  </div>
+                  <div className={cls.skills_item}>
+                      HTML
+                  </div><div className={cls.skills_item}>
+                      CSS
+                  </div><div className={cls.skills_item}>
+                      JavaScript/TypeScript
+                  </div><div className={cls.skills_item}>
+                      SASS
+                  </div><div className={cls.skills_item}>
+                      GITHUB/GITLUB
+                  </div><div className={cls.skills_item}>
+                      Responsive Design
+                  </div>
+
+                  <div className={cls.skills_item}>
+                      Express
+                  </div>
+                  <div className={cls.skills_item}>
+                      RTK Query
+                  </div>
+                   
+              </div>
           </div>
+          </div>
+         
       </div>
   )
 }

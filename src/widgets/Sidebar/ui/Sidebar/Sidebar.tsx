173f6c46323ebd20cffca3ec5e3b53f9029d3d29
Anisina <makhmutovansk@gmail.com>
2023-11-01 08:02:11 +0400

    fix side bar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,43 +1,59 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useState } from 'react'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
-import { Button, ThemeButton } from 'shared/ui/Button/Button'
+// import { Button, ThemeButton } from 'shared/ui/Button/Button'
 import cls from './Sidebar.module.scss'
-import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher'
-import { BugButton } from 'app/providers/ErrorBoundary'
-import CollLeft from 'shared/assets/icons/window-collapse-left-svgrepo-com.svg'
-import CollRight
-  from 'shared/assets/icons/window-collapse-right-svgrepo-com.svg'
+// import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher'
+import { useTranslation } from 'react-i18next'
+import { useTheme } from 'app/providers/ThemeProviders'
+// import { BugButton } from 'app/providers/ErrorBoundary'
+// import CollLeft from 'shared/assets/icons/window-collapse-left-svgrepo-com.svg'
+// import CollRight
+//   from 'shared/assets/icons/window-collapse-right-svgrepo-com.svg'
 
 interface SidebarProps {
   className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(true)
+  // const [collapsed, setCollapsed] = useState(true)
+   const { theme, toggleTheme } = useTheme()
 
-  const onToggle = () => {
-    setCollapsed((prev) => !prev)
+  // const onToggle = () => {
+  //   setCollapsed((prev) => !prev)
+  // }
+  const { t, i18n } = useTranslation()
+
+  const toggle = async () => {
+    void i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
   }
-
   return (
       <div
-            data-testid="sidebar"
-            className={classNames(cls.Sidebar,
-              { [cls.collapsed]: collapsed },
-              [className ?? ''])}
+      data-testid="sidebar"
+      className={classNames(cls.Sidebar)}
+              // { [cls.collapsed]: collapsed },
+              // [className ?? ''])}
         >
-          <Button
+          {/* <Button
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
                 theme={ThemeButton.CLEAR}
             >
               {collapsed ? <CollRight/> : <CollLeft/> }
-          </Button>
+          </Button> */}
           <div className={cls.switchers}>
-              {/* <BugButton/> */}
-              <ThemeSwitcher />
-              <LangSwitcher className={cls.lang} />
+          <div className={cls.social_container}>
+          <ul className={cls.social_icons}>
+        <li><a href="#"><i className="fa fa-whatsapp"></i></a></li>
+        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+        <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i className="fa fa-github"></i></a></li>
+        <li><a href="#" onClick={toggle}><i className="fa fa-language"></i></a></li>
+        <li><a href="#" onClick={toggleTheme}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M297.2 248.9C311.6 228.3 320 203.2 320 176c0-70.7-57.3-128-128-128S64 105.3 64 176c0 27.2 8.4 52.3 22.8 72.9c3.7 5.3 8.1 11.3 12.8 17.7l0 0c12.9 17.7 28.3 38.9 39.8 59.8c10.4 19 15.7 38.8 18.3 57.5H109c-2.2-12-5.9-23.7-11.8-34.5c-9.9-18-22.2-34.9-34.5-51.8l0 0 0 0c-5.2-7.1-10.4-14.2-15.4-21.4C27.6 247.9 16 213.3 16 176C16 78.8 94.8 0 192 0s176 78.8 176 176c0 37.3-11.6 71.9-31.4 100.3c-5 7.2-10.2 14.3-15.4 21.4l0 0 0 0c-12.3 16.8-24.6 33.7-34.5 51.8c-5.9 10.8-9.6 22.5-11.8 34.5H226.4c2.6-18.7 7.9-38.6 18.3-57.5c11.5-20.9 26.9-42.1 39.8-59.8l0 0 0 0 0 0c4.7-6.4 9-12.4 12.7-17.7zM192 128c-26.5 0-48 21.5-48 48c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-44.2 35.8-80 80-80c8.8 0 16 7.2 16 16s-7.2 16-16 16zm0 384c-44.2 0-80-35.8-80-80V416H272v16c0 44.2-35.8 80-80 80z"/></svg></a></li>
+          </ul>
+          </div>
+              {/* <ThemeSwitcher /> */}
+              {/* <LangSwitcher className={cls.lang} /> */}
           </div>
       </div>
   )
